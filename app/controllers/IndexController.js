21fcea737166c1e3cc0d7282722fda6a07ea3b53
Aaron Larner <anlarner@gmail.com>
2014-09-24 19:56:42 -0500

    Add login and registration pages

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -1,13 +1,13 @@
 module.exports = {
 	login: function(req, res, models, config) {
-
+		res.view();
 	},
 
 	logout: function(req, res, models, config) {
-
+		res.view();
 	},
 
 	register: function(req, res, models, config) {
-
+		res.view();
 	}
 };
