ef4261004bcefebf69b283c184eaace88af83dea
Aaron Larner <anlarner@gmail.com>
2015-01-17 22:49:49 -0600

    Remove unnecessary routes and add global navigation

--- config/router.js
+++ config/router.js
@@ -5,12 +5,6 @@ module.exports = {
 	},
 	routes: {
 		'/permission/group/:groupId/:permissionCode': 'PermissionController.groupPermission',
-		'/permission/group/:groupId/:pluginPath/:permissionCode': 'PermissionController.groupPermission',
-		'get /fancy': 'CustomController.fancy',
-		'post /fancy': 'CustomController.postFancy',
-		'put /cstm/:id': 'CustomController.putWithId',
-		'delete /whatwat/:face': 'DeleteController.whatwat',
-		'/hello-world': 'HelloController.world',
-		'/test/override': 'CustomController.override'
+		'/permission/group/:groupId/:pluginPath/:permissionCode': 'PermissionController.groupPermission'
 	}
 };
